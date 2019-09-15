167afc4a5e3fd5bfcac11a68235d6110fdd50a70
Joe <rice.joseph@titans.easternflorida.edu>
2019-09-15 19:50:03 -0400

    Java Program that displays various primitives (data types) such as Int, Long, Double. Also use of wrapper classes to illustrate the range of values allowed with the Int data type.

---
+++ src/com/josephrice/Main.java
@@ -0,0 +1,14 @@
+package com.josephrice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE; // Wrapper Class
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Minimum Value = " + myMaxIntValue);
+
+    }
+}
